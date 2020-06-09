bd61ed78cda01a74beb41274ba745c00b9e3a6dd
SunDoge <384813529@qq.com>
2020-06-09 21:28:39 +0800

    Add unittest for sequential sampler

--- src/samplers/sequential.rs
+++ src/samplers/sequential.rs
@@ -26,13 +26,40 @@ impl Sampler for SequentialSampler {
         if remaining_indices == 0 {
             return None;
         }
-        let mut index_batch: Self::Item = Vec::with_capacity(batch_size.min(remaining_indices));
 
-        for i in &mut index_batch {
-            *i = self.index;
+        let current_batch_size = batch_size.min(remaining_indices);
+        let mut index_batch: Self::Item = Vec::with_capacity(current_batch_size);
+
+        for _ in 0..current_batch_size {
+            index_batch.push(self.index);
             self.index += 1;
         }
 
         Some(index_batch)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample() {
+        let mut sampler = SequentialSampler::new(5);
+
+        assert_eq!(sampler.next(4), Some(vec![0, 1, 2, 3]));
+        assert_eq!(sampler.next(4), Some(vec![4]));
+        assert_eq!(sampler.next(4), None);
+    }
+
+    #[test]
+    fn reset() {
+        let mut sampler = SequentialSampler::new(3);
+        assert_eq!(sampler.next(4), Some(vec![0, 1, 2]));
+        assert_eq!(sampler.next(4), None);
+        sampler.reset(None);
+        assert_eq!(sampler.next(4), Some(vec![0, 1, 2]));
+        sampler.reset(Some(2));
+        assert_eq!(sampler.next(4), Some(vec![0, 1]));
     }
 }
